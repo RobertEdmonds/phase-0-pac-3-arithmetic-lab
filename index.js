5d1911cd71299ab7f21cb39335b0651ee6da8c61
Robert Edmonds <bobby.edmonds89@gmail.com>
2021-11-04 20:39:27 -0600

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,38 @@
+function add(){
+return x + y;
+}
+function subtract(){
+    return x - y;
+}
+function multiply(){
+    return x * y;
+}
+function divide(){
+
+}
+function add(a, b){
+    return a + b;
+}
+function subtract(a, b){
+    return a - b;
+}
+function multiply(a, b){
+    return a * b;
+}
+function divide(a, b){
+    return a / b;
+}
+function increment(n){
+     n++;
+    return n;
+}
+function decrement(n){
+    n--;
+    return n;
+}
+function makeInt(n){
+    return parseInt(n, 10);
+}         
+function preserveDecimal(n){
+    return parseFloat(n);
+}
